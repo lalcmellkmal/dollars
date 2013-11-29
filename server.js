53d3e319506b05c04902053fb8246dae43f6e38e
Lal'C Mellk Mal <lalc@doushio.com>
2013-11-29 13:14:06 -0800

    Random word generation and tmpl rendering

--- server.js
+++ server.js
@@ -1,8 +1,57 @@
 var config = require('./config'),
-    connect = require('connect');
+    connect = require('connect'),
+    fs = require('fs'),
+    Handlebars = require('handlebars');
+
+const WORDS = Object.freeze(require('./words'));
+const INDEX_TMPL = templateSync('tmpl/index.html');
+
+function generateRandomLog() {
+	if (WORDS.length < 41000)
+		throw new Error("Where did my words go?!");
+	function w() { return randomChoice(WORDS); }
+	for (var i = 0; i < 5; i++) {
+		var wwww = [w(), w(), w(), w()];
+		if (allDifferent(wwww))
+			return wwww.join(' ');
+	}
+	throw new Error("Word diversity please!");
+}
+
+function randomChoice(list) {
+	return list[Math.floor(Math.random() * list.length)];
+}
+
+function allDifferent(words) {
+	var n = words.length;
+	for (var i = 0; i < n-1; i++)
+		for (var j = i+1; j < n; j++)
+			if (words[i] == words[j])
+				return false;
+	return true;
+}
+
+function templateSync(filename) {
+	var raw = fs.readFileSync(__dirname + '/' + filename, 'utf8');
+	return Handlebars.compile(raw);
+}
+
+function indexRoute(req, resp, next) {
+	if (req.method == 'GET' && req.url == '/') {
+		var html = INDEX_TMPL({
+			hashString: generateRandomLog(),
+			difficulty: 5,
+		});
+		resp.writeHead(200, {'Content-Type': 'text/html'});
+		resp.end(html);
+		return;
+	}
+	next();
+}
 
 function listen() {
 	var app = connect.createServer();
+	app.use(indexRoute);
 	app.use(connect.static(__dirname + '/www'));
 	app.listen(config.LISTEN_PORT);
 }
